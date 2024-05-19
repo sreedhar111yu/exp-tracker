46a20dc38c8d6dd013dd4d2a1b9691c8271eca2b
sreedhar111yu <sreedharv36@gmail.com>
2024-05-19 18:44:14 +0530

    error occured in db in variable SUM

--- app/(routes)/dashboard/budgets/_components/BudgetList.jsx
+++ app/(routes)/dashboard/budgets/_components/BudgetList.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+'use client'
+import React, { useEffect } from 'react'
 import CreateBudget from './CreateBudget'
+import { db } from '@/utils/dbConfig'
+import { SQL, eq, getTableColumns, sql } from 'drizzle-orm'
+import { Budgets, Expenses } from '@/utils/schema'
+import { useUser } from '@clerk/nextjs'
+
 
 function BudgetList() {
+
+  const{user}=useUser();
+
+  useEffect(()=>{
+   user&&getBudgetList();
+
+  },[user])
+    
+  // used to get budget list
+  const getBudgetList=async()=>{
+
+    const result=await db.select({
+      ...getTableColumns(Budgets),
+      totalSpend:sql`SUM(${Expenses.amount})`.mapWith(Number),
+      totalItem:sql `COUNT(${Expenses.id})`.mapWith(Number)
+    }).from(Budgets)
+    .leftJoin(Expenses,eq(Budgets.id,Expenses.budgetId))
+    .where(eq(Budgets.createdBy,user.primaryEmailAddress?.emailAddress))
+    .groupBy(Budgets.id);
+
+    console.log(result.toString());
+
+  }
   return (
     <div className='mt-7'>
         <div className='grid grid-cols-1 
